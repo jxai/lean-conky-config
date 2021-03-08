04993751ecdec9bad75e413376f2eb9f0fec4490
Jason Xu <769155+jxai@users.noreply.github.com>
2021-03-07 22:56:07 -0800

    Clean code a bit.

--- main.lua
+++ main.lua
@@ -2,57 +2,7 @@ local _dirname_ = debug.getinfo(1, 'S').source:sub(2):match('(.*[/\\])')
 package.path = _dirname_ .. '?.lua;' .. package.path
 utils = require 'utils'
 
--- dynamically show active ifaces
--- see https://matthiaslee.com/dynamically-changing-conky-network-interface/
-local TPL_IFACE =
-[[${if_existing /sys/class/net/<IFACE>/operstate up}]] ..
-[[${voffset 2}${font :size=7}▼${font}  ${downspeed <IFACE>} ${alignc -22}${font :bold:size=8}<IFACE>${font}]] ..
-[[${alignr}${upspeed <IFACE>} ${voffset -2}${font :size=7}▲
-${font}${color lightgray}${downspeedgraph <IFACE> 32,130} ${alignr}${upspeedgraph <IFACE> 32,130 }$color]] ..
-[[${endif}]]
-
-function conky_ifaces()
-    local rendered = {}
-    for i, iface in ipairs(utils.enum_ifaces()) do
-        rendered[i] = TPL_IFACE:gsub('<IFACE>', iface)
-    end
-    if #rendered > 0 then
-        return conky_parse(table.concat(rendered, '\n'))
-    else
-        return conky_parse("${font}(no active network interface found)")
-    end
-end
-
--- dynamically show mounted disks
-local TPL_DISK =
-[[${color}${font :bold:size=8}%s${font} ${alignc -8}%s / %s [%s] ${alignr}%s%%
-${lua_bar 4 percent_ratio %s %s}$color]]
-
-function conky_disks()
-    local rendered = {}
-    for i, disk in ipairs(utils.enum_disks()) do
-        -- human friendly size strings
-        local size_h = utils.filesize(disk.size)
-        local used_h = utils.filesize(disk.used)
-
-        -- get succinct name for the mount
-        local name = disk.mnt
-        local media = name:match('^/media/'..utils.env.USER..'/(.+)$')
-        if media then
-            name = media
-        elseif name == utils.env.HOME then
-            name = '${font :bold:size=11}⌂'
-        end
-        rendered[i] = string.format(TPL_DISK, name, used_h, size_h, disk.type,
-                                    utils.percent_ratio(disk.used, disk.size),
-                                    disk.used, disk.size)
-    end
-    if #rendered > 0 then
-        return conky_parse(table.concat(rendered, '\n'))
-    else
-        return conky_parse("${font}(no mounted disk found)")
-    end
-end
+conky_percent_ratio = utils.percent_ratio
 
 -- unified shortcut to all top_x variables, with optional padding
 function _top_val(ord, dev, type, max_len, align)
@@ -114,4 +64,54 @@ function conky_top_io_line(ord)
     )
 end
 
-conky_percent_ratio = utils.percent_ratio
+-- dynamically show active ifaces
+-- see https://matthiaslee.com/dynamically-changing-conky-network-interface/
+local TPL_IFACE =
+[[${if_existing /sys/class/net/<IFACE>/operstate up}]] ..
+[[${voffset 2}${font :size=7}▼${font}  ${downspeed <IFACE>} ${alignc -22}${font :bold:size=8}<IFACE>${font}]] ..
+[[${alignr}${upspeed <IFACE>} ${voffset -2}${font :size=7}▲${font}
+${color lightgray}${downspeedgraph <IFACE> 32,130} ${alignr}${upspeedgraph <IFACE> 32,130 }${color}]] ..
+[[${endif}]]
+
+function conky_ifaces()
+    local rendered = {}
+    for i, iface in ipairs(utils.enum_ifaces()) do
+        rendered[i] = TPL_IFACE:gsub('<IFACE>', iface)
+    end
+    if #rendered > 0 then
+        return conky_parse(table.concat(rendered, '\n'))
+    else
+        return conky_parse("${font}(no active network interface found)")
+    end
+end
+
+-- dynamically show mounted disks
+local TPL_DISK =
+[[${font :bold:size=8}%s${font} ${alignc -8}%s / %s [%s] ${alignr}%s%%
+${lua_bar 4 percent_ratio %s %s}]]
+
+function conky_disks()
+    local rendered = {}
+    for i, disk in ipairs(utils.enum_disks()) do
+        -- human friendly size strings
+        local size_h = utils.filesize(disk.size)
+        local used_h = utils.filesize(disk.used)
+
+        -- get succinct name for the mount
+        local name = disk.mnt
+        local media = name:match('^/media/'..utils.env.USER..'/(.+)$')
+        if media then
+            name = media
+        elseif name == utils.env.HOME then
+            name = '${font :bold:size=11}⌂'
+        end
+        rendered[i] = string.format(TPL_DISK, name, used_h, size_h, disk.type,
+                                    utils.percent_ratio(disk.used, disk.size),
+                                    disk.used, disk.size)
+    end
+    if #rendered > 0 then
+        return conky_parse(table.concat(rendered, '\n'))
+    else
+        return conky_parse("${font}(no mounted disk found)")
+    end
+end
