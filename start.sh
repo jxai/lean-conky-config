c6f6a0f2ee13f9750339eb77f34872deabba1a1c
Jason Xu <769155+jxai@users.noreply.github.com>
2021-03-30 23:32:18 -0700

    Add custom font for icons and LCD-style time.
    The custom font will be automatically installed while running `start.sh`. It
    relies on FontConfig, in case the custom font is not found in Conky, rendering
    gracefully falls back to use the default font.

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 cd $(dirname $0)
 killall conky 2>/dev/null
+font/install
 echo "Conky waiting 5 seconds to start..."
 if conky --daemonize --quiet --pause=5 --config=./conky.conf ; then
   echo "Started"
