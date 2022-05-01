1204da09aaf6e8e7ff7766edc427aa6a8d1e2dda
Jason Xu <769155+jxai@users.noreply.github.com>
2022-05-01 01:05:00 -0700

    Support re-defining panel in local.conf
    - Simplified access of global T_ and C_
    - Add helper function to build panel
    - Minor adjustment of spacing and panel's min height
    - Updated conf example

--- lib/components/init.lua
+++ lib/components/init.lua
@@ -1,7 +1,12 @@
 local components = {}
+C_ = components -- C_: global alias for `components`
 
 local utils = require("utils")
 local core = require("components.core")
 utils.update_table(components, core)
+
+function components.build_panel()
+    return T_(table.concat(lcc.panel, core.vspace(lcc.config.spacing)))
+end
 
 return components
--- lib/tform.lua
+++ lib/tform.lua
@@ -4,31 +4,32 @@ if _VERSION <= "Lua 5.1" then
     _load = loadstring
 end
 
-local T_ = {}
+local tform = {}
+T_ = tform -- T_: global alias for `tform`
 
 -- scale a number
-function T_.sc(num, scale)
+function tform.sc(num, scale)
     scale = scale or lcc.config.scale
     return num * scale
 end
 
 -- scale then round, for where only integers are allowed
-function T_.sr(num, scale)
-    return math.floor(T_.sc(num, scale) + 0.5)
+function tform.sr(num, scale)
+    return math.floor(tform.sc(num, scale) + 0.5)
 end
 
 -- scale then round to a multiple of 0.5
 -- might be useful for certain cases, e.g. font size
-function T_.sh(num, scale)
-    return math.floor(T_.sc(num * 2, scale) + 0.5) / 2.0
+function tform.sh(num, scale)
+    return math.floor(tform.sc(num * 2, scale) + 0.5) / 2.0
 end
 
 -- apply transform functions to rewrite values in a string
 -- e.g. "$sc{42}" would be replaced by the value of T_.sc(42)
-setmetatable(T_, { __call = function(_, s)
+setmetatable(tform, { __call = function(_, s)
     return s:gsub("$([%w_]+)(%b{})", function(f, args)
         return assert(_load("return T_." .. f .. "(" .. args:sub(2, -2) .. ")"))()
     end)
 end })
 
-return T_
+return tform
