13a53d96750ad5b7abb78258b7e1775aabcf607e
Jason Xu <769155+jxai@users.noreply.github.com>
2022-05-03 01:21:03 -0700

    Support using component function names in panel definition

--- lib/components/init.lua
+++ lib/components/init.lua
@@ -1,12 +1,22 @@
+local utils = require("utils")
+
 local components = {}
 C_ = components -- C_: global alias for `components`
 
-local utils = require("utils")
+-- core components
 local core = require("components.core")
 utils.table.update(components, core)
 
+-- GPU support for different vendors, as of now only Nvidia's is supported
+components.gpu = require("components.gpu")
+
 function components.build_panel()
-    return T_(table.concat(lcc.panel, core.vspace(lcc.config.spacing)))
+    local panel = {}
+    for _, v in ipairs(lcc.panel) do
+        if type(v) == "function" then v = v() end
+        if v ~= nil then table.insert(panel, v) end
+    end
+    return T_(table.concat(panel, core.vspace(lcc.config.spacing)))
 end
 
 return components
