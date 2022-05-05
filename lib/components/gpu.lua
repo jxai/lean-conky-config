ad5122183df796735fedce44636bd74359caffeb
Jason Xu <769155+jxai@users.noreply.github.com>
2022-05-05 01:00:31 -0700

    Modify nvidia component to do dynamic rendering
    and report error if nvidia support is unavailable

--- lib/components/gpu.lua
+++ lib/components/gpu.lua
@@ -3,7 +3,7 @@ local core = require("components.core")
 
 local gpu = {}
 
-local tpl_nvidia =
+local tpl_nvidia_conky =
 utils.tpl [[${font}${nvidia modelname 0} ${alignr} ${nvidia gpuutil 0}%
 ${color3}${nvidiagraph gpuutil $sr{32},$sr{270} 0}${color}
 ${color2}${lua font h2 MEM}${font}${color} ${alignc $sr{-16}}${nvidia memused 0} MB / ${nvidia memmax 0} MB ${alignr}${nvidia memutil 0}%
@@ -11,9 +11,27 @@ ${color3}${nvidiabar $sr{4} memutil 0}${color}
 ${color2}${lua font h2 TEMP}${goto $sr{148}}${lua font h2 FAN}${font}${color}${alignr}${offset $sr{-138}}${nvidia gputemp 0}℃
 ${voffset $sr{-13}}${alignr}${nvidia fanlevel 0}%
 ${color3}${nvidiabar $sr{4},$sr{130} gputemp 0} ${alignr}${nvidiabar $sr{4},$sr{130} fanlevel 0}${color}]]
+local function _nvidia_conky()
+    return T_(tpl_nvidia_conky {})
+end
+
+local _nvidia_enabled = nil
+function conky_nvidia(interv)
+    -- check if conky was built with nvidia support
+    if _nvidia_enabled == nil then
+        _nvidia_enabled = (conky_parse("${nvidia modelname}") ~= "${nvidia}")
+    end
+    if _nvidia_enabled then
+        return core._interval_call(interv, function()
+            return _nvidia_conky()
+        end)
+    else
+        return conky_parse(core.message("error+", "Conky not built with nvidia support"))
+    end
+end
+
 function gpu.nvidia(args)
-    local top_n = utils.table.get(args, 'top_n', 5)
-    return core.section("GPU", "") .. "\n" .. tpl_nvidia {}
+    return core.section("GPU", "") .. "\n" .. [[${lua nvidia 5}]]
 end
 
 return gpu
