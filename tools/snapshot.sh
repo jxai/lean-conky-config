f903c0caadb730246ffdc0e73dadf38af458812c
Jason Xu <769155+jxai@users.noreply.github.com>
2024-09-05 01:05:04 -0700

    Update snapshot script to capture LCC window blended with desktop

--- tools/snapshot.sh
+++ tools/snapshot.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 # vim: ft=sh:ts=4:sw=4:et:ai:cin
 
+opt="$1"
 window="conky-lcc"
-out_file="lcc.$(date +%H%M%S).png"
+out_file="lcc.$(date +%y%m%d-%H%M%S).png"
+
+snap_lcc() {
+    if [ "$opt" = "-c" ]; then
+        # conky window only (black background)
+        import -window ${window} ${out_file}
+    else
+        # desktop background blended with conky
+        region=$(xwininfo -name "$window" -shape | perl -0pe "s/^.*corners\:\s+([^\s]+).*geometry\s+(\d+x\d+).*$/\2\1/si")
+        import -window root -crop ${region} ${out_file}
+    fi
+
+}
+
 xdotool search --name ${window} >/dev/null &&
-    import -window ${window} ${out_file} &&
+    snap_lcc &&
     echo "snapped to: ${out_file}" &&
     feh ${out_file}
